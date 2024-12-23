101c136eb3d8b4a92f44bf460b96c91f1963633a
Steven Confessore <confessore@balasolu.com>
2024-12-23 09:23:47 -0500

    egui

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use bevy::{color::palettes::basic::*, prelude::*};
+use bevy::{color::palettes::basic::*, prelude::*, ui};
 use neargate::{
     Cell, Equippable, Game, GameState, Item, JobType, Spell, Unit, AURAS, CONSUMABLES, SPELLS,
 };
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy::{prelude::*, window::PrimaryWindow, winit::WinitSettings};
 
 /*fn main() {
     let mut warrior = Unit::new("Warrior");
@@ -78,11 +80,14 @@ fn main() {
     App::new()
         .init_resource::<Game>()
         .add_plugins(DefaultPlugins)
+        .add_plugins(EguiPlugin)
+        .init_resource::<OccupiedScreenSpace>()
         .init_state::<GameState>()
         .enable_state_scoped_entities::<GameState>()
-        .add_systems(Startup, setup_cameras)
-        .add_systems(Startup, setup_ui)
-        .add_systems(Update, button_system)
+        //.add_systems(Startup, setup_cameras)
+        .add_systems(Startup, setup_system)
+        .add_systems(Update, ui_example_system)
+        .add_systems(Update, update_camera_transform_system)
         .add_systems(OnEnter(GameState::Playing), setup)
         .run();
 }
@@ -153,272 +158,133 @@ const RESET_FOCUS: [f32; 3] = [
 #[derive(Resource, Deref, DerefMut)]
 struct Random(ChaCha8Rng);
 
-const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
-
-fn button_system(
-    mut interaction_query: Query<
-        (
-            &Interaction,
-            &mut BackgroundColor,
-            &mut BorderColor,
-            &Children,
-        ),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-) {
-    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Pressed => {
-                **text = "Press".to_string();
-                *color = PRESSED_BUTTON.into();
-                border_color.0 = RED.into();
-            }
-            Interaction::Hovered => {
-                **text = "Hover".to_string();
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                **text = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
-            }
-        }
-    }
+#[derive(Default, Resource)]
+struct OccupiedScreenSpace {
+    left: f32,
+    top: f32,
+    right: f32,
+    bottom: f32,
 }
 
-fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands
-        .spawn(Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            justify_content: JustifyContent::End,
-            align_items: AlignItems::Start,
-            ..default()
-        })
-        .with_children(|parent| {
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-        });
+const CAMERA_TARGET: Vec3 = Vec3::ZERO;
 
-    commands
-        .spawn(Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            align_items: AlignItems::End,
-            justify_content: JustifyContent::Start,
-            ..default()
+#[derive(Resource, Deref, DerefMut)]
+struct OriginalCameraTransform(Transform);
+
+
+fn update_camera_transform_system(
+    occupied_screen_space: Res<OccupiedScreenSpace>,
+    original_camera_transform: Res<OriginalCameraTransform>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut camera_query: Query<(&Projection, &mut Transform)>,
+) {
+    let (camera_projection, mut transform) = match camera_query.get_single_mut() {
+        Ok((Projection::Perspective(projection), transform)) => (projection, transform),
+        _ => unreachable!(),
+    };
+
+    let distance_to_target = (CAMERA_TARGET - original_camera_transform.translation).length();
+    let frustum_height = 2.0 * distance_to_target * (camera_projection.fov * 0.5).tan();
+    let frustum_width = frustum_height * camera_projection.aspect_ratio;
+
+    let window = windows.single();
+
+    let left_taken = occupied_screen_space.left / window.width();
+    let right_taken = occupied_screen_space.right / window.width();
+    let top_taken = occupied_screen_space.top / window.height();
+    let bottom_taken = occupied_screen_space.bottom / window.height();
+    transform.translation = original_camera_transform.translation
+        + transform.rotation.mul_vec3(Vec3::new(
+            (right_taken - left_taken) * frustum_width * 0.5,
+            (top_taken - bottom_taken) * frustum_height * 0.5,
+            0.0,
+        ));
+}
+
+fn setup_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+    commands.spawn((
+        PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+
+    let camera_pos = Vec3::new(-2.0, 2.5, 5.0);
+    let camera_transform =
+        Transform::from_translation(camera_pos).looking_at(CAMERA_TARGET, Vec3::Y);
+    commands.insert_resource(OriginalCameraTransform(camera_transform));
+
+    commands.spawn((Camera3d::default(), camera_transform));
+}
+
+fn ui_example_system(
+    mut is_last_selected: Local<bool>,
+    mut contexts: EguiContexts,
+    mut occupied_screen_space: ResMut<OccupiedScreenSpace>,
+) {
+    let ctx = contexts.ctx_mut();
+
+    occupied_screen_space.left = egui::SidePanel::left("left_panel")
+        .resizable(true)
+        .show(ctx, |ui| {
+            ui.label("Left resizeable panel");
+            if ui
+                .add(egui::widgets::Button::new("A button").selected(!*is_last_selected))
+                .clicked()
+            {
+                *is_last_selected = false;
+            }
+            if ui
+                .add(egui::widgets::Button::new("Another button").selected(*is_last_selected))
+                .clicked()
+            {
+                *is_last_selected = true;
+            }
+            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
         })
-        .with_children(|parent| {
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        // horizontally center child text
-                        justify_content: JustifyContent::Center,
-                        // vertically center child text
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BorderColor(Color::BLACK),
-                    BorderRadius::MAX,
-                    BackgroundColor(NORMAL_BUTTON),
-                ))
-                .with_child((
-                    Text::new("Button"),
-                    TextFont {
-                        font: asset_server.load("FiraSans-Bold.ttf"),
-                        font_size: 33.0,
-                        ..default()
-                    },
-                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                ));
-        });
+        .response
+        .rect
+        .width();
+    occupied_screen_space.right = egui::SidePanel::right("right_panel")
+        .resizable(true)
+        .show(ctx, |ui| {
+            ui.label("Right resizeable panel");
+            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+        })
+        .response
+        .rect
+        .width();
+    occupied_screen_space.top = egui::TopBottomPanel::top("top_panel")
+        .resizable(true)
+        .show(ctx, |ui| {
+            ui.label("Top resizeable panel");
+            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+        })
+        .response
+        .rect
+        .height();
+    occupied_screen_space.bottom = egui::TopBottomPanel::bottom("bottom_panel")
+        .resizable(true)
+        .show(ctx, |ui| {
+            ui.label("Bottom resizeable panel");
+            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+        })
+        .response
+        .rect
+        .height();
 }
